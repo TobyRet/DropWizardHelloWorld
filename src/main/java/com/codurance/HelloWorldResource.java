f584a79b2d297512cd79e7cf0e868841c50ed2f6
Toby Retallick <toby.retallick@gmail.com>
2015-02-19 09:17:54 +0000

    Created basic Hello World service with Drop Wizard

---
+++ src/main/java/com/codurance/HelloWorldResource.java
@@ -0,0 +1,13 @@
+package com.codurance;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/greeting/{name}")
+public class HelloWorldResource {
+	@GET
+	public String getGreeting(@PathParam("name") String name) {
+		return "Hello " + name + "!";
+	}
+}
